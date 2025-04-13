ae26fc03079c3364181e372e0a5b6e50cac65908
marrrtu-maker <justfortwitter.martu@gmail.com>
2025-04-13 16:43:09 -0300

    00 sintaxis,variables,etc

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/marrrtu-maker.py
@@ -0,0 +1,12 @@
+# https://www.python.org
+# comentario de una linea
+""" comentario de 
+varias lineas"""
+my_variable = 15 
+MY_CONSTANT = "bla bla bla"
+my_int= 03
+my_float= 3,89
+my_bool= True
+my_str= "texto texto texto"
+my_other_string= 'mi otra cadena de texto'
+print ("Hola, Python")
